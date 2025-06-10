1cbbb63506d7816077ff7ca7992c478a869dd5c2
wltkdgur123@naver.com <wltkdgur123@naver.com>
2025-06-10 18:55:41 +0900

    feat.PromptHeader 제작 및 chatHistory 헤더와  연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home/Home.jsx";
 import Prompt from "./pages/Prompt/Prompt.jsx";
@@ -9,15 +14,19 @@ import ReservationsPage from "./pages/ReservationsPage/ReservationsPage.jsx";
 import HistoryPage from "./pages/HistoryPage/HistoryPage.jsx";
 import AnalysisPage from "./pages/AnalysisPage/AnalysisPage.jsx";
 import "./App.css";
+import use400px from "./hooks/use400px";
 
 function App() {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+  const is400px = use400px();
+  const location = useLocation();
+
+  // Prompt 페이지이고 400px 이하일 때는 Header를 숨김
+  const hideHeader = location.pathname === "/prompt" && is400px;
 
   return (
-    <Router>
-      <Header
-        onSignUpClick={() => setIsSignUpOpen(true)}
-      />
+    <>
+      {!hideHeader && <Header onSignUpClick={() => setIsSignUpOpen(true)} />}
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -29,13 +38,18 @@ function App() {
         </Routes>
       </div>
       {isSignUpOpen && (
-        <SignUp
-          isOpen={isSignUpOpen}
-          onClose={() => setIsSignUpOpen(false)}
-        />
+        <SignUp isOpen={isSignUpOpen} onClose={() => setIsSignUpOpen(false)} />
       )}
+    </>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
     </Router>
   );
 }
 
-export default App; 
+export default AppWrapper;
