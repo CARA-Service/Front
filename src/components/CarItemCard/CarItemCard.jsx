5092ba644df2e2f9e65e4850f43e48ff5050c496
HanGyuDeok <gyudeok0202@gmail.com>
2025-06-12 17:35:01 +0900

    feat: 자동차 카드 컴포넌트 UI 개선 - 주행거리 계산 로직 수정 (1,000km 기준) - 태그 호버 효과 색상 변경 - 툴팁 메시지 추가

--- src/components/CarItemCard/CarItemCard.jsx
+++ src/components/CarItemCard/CarItemCard.jsx
@@ -13,7 +13,7 @@ const CarItemCard = ({ car }) => {
   } = car;
 
   const calculateCostPerKm = (price) => {
-    const baseCost = price / 10000; // 기본 주행거리 10,000km로 고정
+    const baseCost = price / 1000; // 기본 주행거리 1,000km로 고정
     const minCost = Math.floor(baseCost * 0.85);
     const maxCost = Math.ceil(baseCost * 1.15);
     return `${minCost} ~ ${maxCost}`;
@@ -48,6 +48,11 @@ const CarItemCard = ({ car }) => {
             e.target.src = './default-profile.png';
           }}
         />
+        <div className="zoom-icon">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z" fill="currentColor"/>
+          </svg>
+        </div>
       </div>
       <div className="car-info">
         <p className="car-comment">{comment}</p>
