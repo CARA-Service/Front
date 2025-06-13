efe8d649d61882a2964f84585ffa76ca6dea1f51
minseong0123 <minseong0213@naver.com>
2025-06-13 21:20:33 +0900

    fix : car 중복리스트 제거 및 app.jsx 헤더추가가

--- src/pages/CarListPage/CarListPage.jsx
+++ src/pages/CarListPage/CarListPage.jsx
@@ -56,55 +56,6 @@ const CarListPage = () => {
   }
 ];
 
-    {
-      car_id: 1,
-      agency_id: 101,
-      model_name: '엘란트라',
-      manufacturer: '현대',
-      category: '세단',
-      capacity: 8,
-      luggage_size: '470L',
-      fuel_type: '가솔린',
-      fuel_efficiency: '40.2',
-      daily_price: 185000,
-      image_url: './현대엘란트라세단(측면).png',
-      additional_options: ['후방센서', '블루투스', '자동주차'],
-      discountRate: 20,
-      comment: '1일 이상 운행하면 20% 페이백 !',
-    },
-    {
-      car_id: 2,
-      agency_id: 102,
-      model_name: '코롤라',
-      manufacturer: '도요타',
-      category: '세단',
-      capacity: 2,
-      luggage_size: '430L',
-      fuel_type: '하이브리드',
-      fuel_efficiency: '9.3',
-      daily_price: 222000,
-      image_url: './도요타COROLLA(측면).png',
-      additional_options: ['후방카메라', '스마트키', '블루투스'],
-      discountRate: 15,
-      comment: ' ⚠️ 사고 차량입니다 주의 요망 ',
-    },
-    {
-      car_id: 3,
-      agency_id: 103,
-      model_name: 'C클래스',
-      manufacturer: '벤츠',
-      category: '세단',
-      capacity: 5,
-      luggage_size: '455L',
-      fuel_type: '디젤',
-      fuel_efficiency: '1.2',
-      daily_price: 528000,
-      image_url: './벤츠메르세데스C(측면).png',
-      additional_options: ['파노라마루프', '헤드업디스플레이', '자율주행'],
-      discountRate: -140,
-      comment: '많은 사용자들이 이용한 차량입니다!',
-    }
-  ];
 
   return (
     <>
